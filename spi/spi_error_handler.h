b489580da254cbe49eb7f06a62646aac2f90440d
ddening <44156662+ddening@users.noreply.github.com>
2022-08-10 14:49:45 +0200

    Update spi_error_handler.h

--- spi/spi_error_handler.h
+++ spi/spi_error_handler.h
@@ -29,7 +29,7 @@
 *
 * ERROR LED SEQUENCE:
 *
-*	SPI_BUFFER_OVERFLOW			| SHORT, SHORT, SHORT
+*	SPI_BUFFER_OVERFLOW         | SHORT, SHORT, SHORT
 *	----------------------------|---------------------
 *	SPI_BUFFER_DATA_OVERWRITE   | SHORT, SHORT, LONG 
 *	----------------------------|---------------------
@@ -37,49 +37,49 @@
 *	----------------------------|---------------------
 *	SPI_INVALID_PORT            | SHORT, LONG , LONG
 *	----------------------------|---------------------
-*	SPI_WRITE_COLLISION			| LONG , SHORT, SHORT
+*	SPI_WRITE_COLLISION         | LONG , SHORT, SHORT
 *	----------------------------|---------------------
-*	SPI_FLUSH_FAILED			| LONG , SHORT, LONG
+*	SPI_FLUSH_FAILED            | LONG , SHORT, LONG
 *	----------------------------|---------------------
 *	SPI_RECV_BUSY				| LONG , LONG , SHORT
 *	----------------------------|---------------------
-*	SPI_NOT_DEFINED				| LONG , LONG , LONG
+*	SPI_NOT_DEFINED             | LONG , LONG , LONG
 *	
 * ERRORS
 *
-*	SPI_BUFFER_OVERFLOW:
-*		1.	More data written to the buffer than allowed.
-*			Change TX_BUFFER_SIZE in the <spi_buffer.h> if needed.
+*   SPI_BUFFER_OVERFLOW:
+*		1.  More data written to the buffer than allowed.
+*           Change TX_BUFFER_SIZE in the <spi_buffer.h> if needed.
 *		2.  More bytes requested than the rx_buffer can store.
-*			Change RX_BUFFER_SIZE in the <spi_buffer.h> if needed.
+*           Change RX_BUFFER_SIZE in the <spi_buffer.h> if needed.
 *
-*	SPI_BUFFER_DATA_OVERWRITE:
-*		If tasks are created faster than the MCU can process current tasks in the buffer, 
-*		data will be overwritten. 	
-*		Increase system frequency or TX_BUFFER size or SPI_CLOCK_DIVx.
+*   SPI_BUFFER_DATA_OVERWRITE:
+*       If tasks are created faster than the MCU can process current tasks in the buffer, 
+*       data will be overwritten. 	
+*       Increase system frequency or TX_BUFFER size or SPI_CLOCK_DIVx.
 *
-*	SPI_DATA_OVERFLOW:
-*		More data written to the data buffer than allowed.
-*		Change DX_BUFFER_SIZE in the <spi_buffer.h> if needed.
+*   SPI_DATA_OVERFLOW:
+*       More data written to the data buffer than allowed.
+*       Change DX_BUFFER_SIZE in the <spi_buffer.h> if needed.
 *
-*	SPI_INVALID_PORT:
-*		Certain ports are reserved for SPI functionality. These are not allowed
-*		for slave devices. These ports can change depending on the MCU used.
-*		Refer to the <spi_io.h> to see which ports are used.
+*   SPI_INVALID_PORT:
+*       Certain ports are reserved for SPI functionality. These are not allowed
+*       for slave devices. These ports can change depending on the MCU used.
+*       Refer to the <spi_io.h> to see which ports are used.
 *
-*	SPI_WRITE_COLLISION:
-*		If the SPI Data Register (SPDR) is written to during a data transfer
-*		the WCOL bit is set in the SPSR.
+*   SPI_WRITE_COLLISION:
+*       If the SPI Data Register (SPDR) is written to during a data transfer
+*       the WCOL bit is set in the SPSR.
 *
-*	SPI_FLUSH_FAILED:
-*		The flush command was executed while the SPI was still active.
+*   SPI_FLUSH_FAILED:
+*       The flush command was executed while the SPI was still active.
 *
-*	SPI_RECV_BUSY:
-*		The SPI is currently busy sending other data and
-*		can't initiate another receive command.
+*   SPI_RECV_BUSY:
+*       The SPI is currently busy sending other data and
+*       can't initiate another receive command.
 *
-*	SPI_NOT_DEFINED:
-*		Errors that are either not defined yet or are general errors (e.g. memory allocation failure).
+*   SPI_NOT_DEFINED:
+*       Errors that are either not defined yet or are general errors (e.g. memory allocation failure).
 *************************************************************************/
 
 /**
@@ -114,11 +114,11 @@ typedef enum spi_error_t {
 } spi_error_t;
 
 /**
- * @brief	Prototype of an error handler.
+ * @brief   Prototype of an error handler.
  *
  * If an error occurs, a specific led sequence is shown on the STK600.
  *
- * @return	Returns an error code.
+ * @return  Returns an error code.
  */
 spi_error_t error_handler(spi_error_t error);
 
